0ada71bdecbdb7edd311320497e3e88abacc3476
Yurii-Kovalenko <yuriy.kovalenko.in@gmail.com>
2024-10-11 18:53:41 +0300

    09: UPD

--- quotes/quotes/settings.py
+++ quotes/quotes/settings.py
@@ -48,9 +48,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quotes.quoteapp',
-    'quotes.users'
+    'quoteapp',
+    'users'
 ]
+
+#    'quotes.quoteapp',
+#    'quotes.users'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
